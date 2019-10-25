d695772ab9036c5374532a5d9e78ec3bfb722f01
istruthy <daren.lunsford+istruthy@gmail.com>
2019-10-25 00:25:33 -0500

    feat(dashboard): add idenity provider

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { IdentityContextProvider } from 'react-netlify-identity-widget';
 
 import './layout.css';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <IdentityContextProvider url="https://istruthy-jamstack-dashboard.netlify.com">
       <header>
         <Link to="/">Jamstack App</Link>
       </header>
 
       <main>{children}</main>
-    </>
+    </IdentityContextProvider>
   );
 };
 
